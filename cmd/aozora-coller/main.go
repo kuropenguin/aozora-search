52b4bb0f3896b4c6b2ea064c9edcdf09c2e926de
kuropenguin <kuro1994.t@gmail.com>
2023-03-19 15:59:05 +0900

    スクレイピングして、リストを取得するように

--- cmd/aozora-coller/main.go
+++ cmd/aozora-coller/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Entry struct {
@@ -15,12 +17,20 @@ type Entry struct {
 }
 
 func findEntities(siteURL string) ([]Entry, error) {
+	doc, err := goquery.NewDocument(siteURL)
+	if err != nil {
+		return nil, err
+	}
+	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
+		println(elem.Text(), elem.AttrOr("href", ""))
+		println(n)
+	})
 	// 処理
 	return nil, nil
 }
 
 func main() {
-	listURL := "https://www.aozora.gr.jp/index_pages/person1257.html"
+	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
 	entries, err := findEntities(listURL)
 	if err != nil {
 		log.Fatal(err)
